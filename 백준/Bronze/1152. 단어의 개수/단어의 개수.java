30ab2bc14d912acceb709e3131eeae7e6471afcd
dohaahi <107530593+dohaahi@users.noreply.github.com>
2023-05-19 23:39:33 +0900

    [Bronze II] Title: 단어의 개수, Time: 512 ms, Memory: 38644 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        String[] str = sc.nextLine().trim().split(" ");
+        for (int i = 0; i < str.length; i++) {
+            if (!str[i].equals("")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
