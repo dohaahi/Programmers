fe08c2f6c859470d9a4e979ae1c239a9ea0c488c
dohaahi <107530593+dohaahi@users.noreply.github.com>
2023-05-29 23:31:55 +0900

    [level 0] Title: 숫자 찾기, Time: 0.12 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num, int k) {
+        String[] arr = Integer.toString(num).split("");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(Integer.toString(k))) {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
