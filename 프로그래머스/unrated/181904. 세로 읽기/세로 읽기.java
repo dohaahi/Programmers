a042e9dcb3b4cae5b415d9100e5d432c9aee81ed
dohaahi <107530593+dohaahi@users.noreply.github.com>
2023-06-21 15:06:53 +0900

    [unrated] Title: 세로 읽기, Time: 2.47 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+
+        String[] arr = my_string.split("");
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            answer += arr[i];
+        }
+        return answer;
+    }
+}
