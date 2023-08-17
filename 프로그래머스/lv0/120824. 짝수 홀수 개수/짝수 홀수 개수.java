130c0fdd99062027b8c3ec371f59277dd5f05306
dohaahi <107530593+dohaahi@users.noreply.github.com>
2023-08-17 20:00:47 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.89 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,13 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
-        for(int num : num_list) {
-            if(num % 2 == 0) {
-                answer[0] += 1;
-            } else {
-                            answer[1] += 1;
-            }
-        }
+
+        int even = Long.valueOf(Arrays.stream(num_list).filter(i -> i % 2 == 0).count()).intValue();
+        answer[0] = even;
+        answer[1] = num_list.length - even;
+
         return answer;
     }
 }
