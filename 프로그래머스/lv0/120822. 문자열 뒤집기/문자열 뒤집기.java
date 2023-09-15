d0cd085f99e9a4d02e7a918b3fc69cbf2cce4a9e
dohaahi <107530593+dohaahi@users.noreply.github.com>
2023-09-15 11:13:27 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String[] arr = my_string.split("");
-        for(int i=arr.length-1; i>=0; i--) {
-            answer += arr[i];
-        }
-        return answer;
+              return new StringBuilder(my_string).reverse().toString();
+
     }
 }
