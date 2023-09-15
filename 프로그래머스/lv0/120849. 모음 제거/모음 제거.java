f072ff13ca656e945b679093ecfcce1696b3db03
dohaahi <107530593+dohaahi@users.noreply.github.com>
2023-09-15 11:16:58 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,15 +1,9 @@
 class Solution {
    public static String solution(String my_string) {
-        String[] vowel = {"a", "e", "i", "o", "u"};
-        String answer = "";
-
-        for (String s : my_string.split("")) {
-            if (s.contains("a") || s.contains("e") || s.contains("i") || s.contains("o") || s.contains("u")) {
-                continue;
-            }
-            answer += s;
-        }
-
-        return answer;
+       return my_string.replace("a", "")
+                .replace("e", "")
+                .replace("i", "")
+                .replace("o", "")
+                .replace("u", "");
     }
 }
