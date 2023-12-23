14b86e384978cc1bf2cf85ca00c3700ba680037b
dohaahi <107530593+dohaahi@users.noreply.github.com>
2023-12-23 15:31:40 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,10 +1,7 @@
 def solution(num_list, n):
     answer = []
 
-    for i in range(0,len(num_list),n):
-        listt = []
-        for j in range(i,i+n):
-            listt.append(num_list[j])
-        answer.append( listt)
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i + n])
 
     return answer
