f1fc6d80bc544566610fee0137bd78da24c81e1e
dohaahi <107530593+dohaahi@users.noreply.github.com>
2023-12-24 15:19:22 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,8 +1,5 @@
 def solution(arr, queries):
-    for q in queries:
-        a = arr[q[0]]
-        b = arr[q[1]]
-        arr[q[1]] = a
-        arr[q[0]] = b
+    for a,b in queries:
+        arr[a],arr[b] = arr[b], arr[a]
 
     return arr
