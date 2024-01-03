42caf7a63ec9524696d13d560b5069b229dde05c
dohaahi <107530593+dohaahi@users.noreply.github.com>
2024-01-03 22:42:43 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    new = my_string.split(' ')
+    answer = int(new[0])
+
+    for i in range(1, len(new), 2):
+        if new[i] == '+':
+            answer += int(new[i + 1])
+        else:
+            answer -= int(new[i + 1])
+    
+    return answer
