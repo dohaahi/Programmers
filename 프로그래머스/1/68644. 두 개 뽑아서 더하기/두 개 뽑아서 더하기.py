b164ba03714a55589decfd4416a5ab4e6937af68
dohaahi <107530593+dohaahi@users.noreply.github.com>
2024-03-12 14:02:50 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.12 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = []
+    
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i != j:
+                answer.append(numbers[i] + numbers[j])
+    
+    return sorted(set(answer))
